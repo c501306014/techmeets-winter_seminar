b1f258731e5b5085880e4532a1c6bb9762ef4487
c501306014 <c501306014@gmail.com>
2020-12-25 11:02:32 +0900

    課題レベル１　タイトルの設定

--- single.php
+++ single.php
@@ -3,7 +3,7 @@
 <?php if(have_posts()): while(have_posts()) : the_post(); ?>
 
 <div class="post-title">
-    <h1><?php the_title(); ?></h1>
+    <h1><?php bloginfo('name'); wp_title('|', true, 'left'); ?></h1>
 </div>
 
 <!-- パンくずメニュー -->
